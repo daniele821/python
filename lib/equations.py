575d5313a167760da15add04200ecf6c4020103c
Daniele Muffato <danixgithub1@gmail.com>
2024-04-26 15:17:55 +0200

    more fixes

--- lib/equations.py
+++ lib/equations.py
@@ -3,10 +3,14 @@
 import numexpr
 
 
-def find_operators(operators=['+', '-', '*', '/'], nums=[8, 1, 1, 5]):
+def find_operators(operators=['+', '-', '*', '/'], nums=[8, 1, 1, 5], iterate_orders=True):
     """
-        - operators must be a list of strings of a single char
-        - nums must be a list of numbers of a single digit
+        - operators must be a list of strings
+            - specify all valid operators to fill in the gaps
+        - nums must be a list of numbers
+            - numbers making up the left side of the equation
+        - iterate_orders must be a boolean
+            - enable iterating all possible positions of a pair of brackets
     """
 
     optypes = len(operators)
@@ -23,19 +27,28 @@ def find_operators(operators=['+', '-', '*', '/'], nums=[8, 1, 1, 5]):
             iter[i] = (num // ops**i) % optypes
 
         # build up the nth expression as a string
-        print('----------------------------')
-        expr = str(nums[0])
+        if iterate_orders:
+            print('----------------------------')
+        expr = [str(nums[0])]
         for i in range(1, ops):
-            expr += operators[iter[i-1]]
-            expr += str(nums[i])
+            expr.append(operators[iter[i-1]])
+            expr.append(str(nums[i]))
+        expr_str = ''.join(expr)
+        try:
+            res = numexpr.evaluate(expr_str)
+        except Exception:
+            res = None
+        print(expr_str, '=', res)
 
         # for each nth expression, build all possible () positioning
-        for a in range(ops):
-            for b in range(a, ops):
-                expr2 = expr[:b*2+1]+')'+expr[b*2+1:]
-                expr2 = expr2[:a*2]+'('+expr2[a*2:]
-                try:
-                    res = numexpr.evaluate(expr2)
-                except Exception:
-                    res = None
-                print(expr2, '=', res)
+        if iterate_orders:
+            for a in range(ops):
+                for b in range(a, ops):
+                    expr2 = expr[:b*2+1]+[')']+expr[b*2+1:]
+                    expr2 = expr2[:a*2]+['(']+expr2[a*2:]
+                    expr2_str = ''.join(expr2)
+                    try:
+                        res = numexpr.evaluate(expr2_str)
+                    except Exception:
+                        res = None
+                    print(expr2_str, '=', res)
