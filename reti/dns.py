58e84c841edf3455ed83293b2852892315edea17
Daniele Muffato <danixgithub1@gmail.com>
2024-05-06 17:13:49 +0200

    works!

--- reti/dns.py
+++ reti/dns.py
@@ -10,9 +10,17 @@ def create_dns_message(domain_name: str) -> bytes:
     QDcount = bytes([0x00, 0x01])
     ANcount = bytes([0x00, 0x00])
     NScount = bytes([0x00, 0x00])
-    ARcount = bytes([0x00, 0x01])
+    ARcount = bytes([0x00, 0x00])
     flags = bytes([0x01, 0x20])
-    return identifier + flags + QDcount + ANcount + NScount + ARcount
+    res = identifier + flags + QDcount + ANcount + NScount + ARcount
+    domains = domain_name.split('.')
+    for domain in domains:
+        res += bytes([len(domain)])
+        res += bytes(domain.encode())
+    res += bytes([0x00])
+    res += bytes([0x00, 0x01])
+    res += bytes([0x00, 0x01])
+    return res
 
 
 def send_dns_message(dns_message: bytes):
@@ -21,6 +29,8 @@ def send_dns_message(dns_message: bytes):
     resolver_addr = (resolver_ip, resolver_port)
     socket = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
     socket.sendto(dns_message, resolver_addr)
+    recv = socket.recv(2048)
+    print(recv.hex())
     socket.close()
 
 
