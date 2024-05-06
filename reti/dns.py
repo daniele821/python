84dfa9ce154d2c315fca99146b3aa053105ad49a
Daniele Muffato <danixgithub1@gmail.com>
2024-05-06 17:14:54 +0200

    prints

--- reti/dns.py
+++ reti/dns.py
@@ -28,6 +28,7 @@ def send_dns_message(dns_message: bytes):
     resolver_port = 53
     resolver_addr = (resolver_ip, resolver_port)
     socket = sk.socket(sk.AF_INET, sk.SOCK_DGRAM)
+    print(dns_message.hex())
     socket.sendto(dns_message, resolver_addr)
     recv = socket.recv(2048)
     print(recv.hex())
