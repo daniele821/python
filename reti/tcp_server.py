9eab6653834100aa4941166a69f1a1875915666f
Daniele Muffato <danixgithub1@gmail.com>
2024-05-07 13:45:20 +0200

    small changes

--- reti/tcp_server.py
+++ reti/tcp_server.py
@@ -4,17 +4,18 @@ import lib.ipaddr as ipaddr
 import socket as sk
 
 ip = ipaddr.get_ip()
-port = 50_000
+port = 51_000
 addr = (ip, port)
 
 socket = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
 socket.bind(addr)
 socket.listen(1)
+print('listing on address:', addr)
 
 try:
     while True:
-        print('listing on address:', addr)
         stream, raddr = socket.accept()
+        print('established connection with:', raddr)
 except KeyboardInterrupt:
     print('closing server')
     socket.close()
