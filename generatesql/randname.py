25cf788f97d98726f06851e7ebaf83fd34a7319e
Daniele Muffato <danixgithub1@gmail.com>
2024-06-26 00:00:49 +0200

    autogenerate names

--- generatesql/randname.py
+++ generatesql/randname.py
@@ -1,5 +1,7 @@
 #!/bin/python3
 
+import random
+
 with open('names/namesF') as f:
     fname = [x.strip() for x in f.read().splitlines()]
 
@@ -9,6 +11,11 @@ with open('names/namesM') as f:
 with open('names/surnames') as f:
     surname = [x.strip() for x in f.read().splitlines()]
 
-    print(fname)
-    print(mname)
-    print(surname)
+res = set()
+while len(res) < 50:
+    res.add((random.choice(fname), random.choice(surname), 'F'))
+while len(res) < 100:
+    res.add((random.choice(mname), random.choice(surname), 'M'))
+
+for name, surname, sex in res:
+    print(name, surname, sex)
