9970e3d780870799ef843a156bdeda96c2972884
Daniele Muffato <danixgithub1@gmail.com>
2024-06-26 11:13:11 +0200

    script to get random cf

--- generatesql/info/randompasseggero.py
+++ generatesql/info/randompasseggero.py
@@ -1,1 +1,19 @@
 #!/bin/python3
+
+import sys
+import random
+
+try:
+    LENGTH = int(sys.argv[1])
+except:
+    LENGTH = 20
+
+with open('passeggeri') as f:
+    listCF = [x.strip() for x in f.read().splitlines()]
+
+res = set()
+while len(res) < LENGTH:
+    res.add(random.choice(listCF))
+
+for elem in res:
+    print(elem)
