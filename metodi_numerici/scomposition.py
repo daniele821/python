8f5c1df8471f544618cfa82e396263c1d8bf83e2
Daniele Muffato <danixgithub1@gmail.com>
2024-07-19 10:41:16 +0200

    added gauss-seidel implementation

--- metodi_numerici/scomposition.py
+++ metodi_numerici/scomposition.py
@@ -17,12 +17,37 @@ def jacobi(A, b, x0, toll, itmax):
 
     # check for convergence
     if np.max(np.linalg.eigvals(T)) >= 1:
-        raise ("Jacobbi cannot converge")
+        raise ValueError("Jacobbi cannot converge")
 
-    # iterate to the solution
+    # iterate toward the solution
     while it < itmax and error > toll:
         x = T@x0 + invM@b
         error = np.linalg.norm(x - x0) / (np.linalg.norm(x))
+        x0 = x.copy()
+        it += 1
+
+    return x, it
+
+
+def gauss_seidel(A, b, x0, toll, itmax):
+    D = np.diag(np.diag(A))
+    E = np.tril(A, -1)
+    F = np.triu(A, 1)
+    M = D + E
+    invM = np.linalg.inv(M)
+    N = -F
+    T = invM @ N
+    it = 1
+    error = np.inf
+
+    # check for convergence
+    if (np.max(np.linalg.eigvals(T)) >= 1):
+        raise ValueError("Gauss-seidel cannot converge")
+
+    # iterate toward the solution
+    while it < itmax and error > toll:
+        x, _ = solve_triangular.Lsolve(M, N@x0+b)
+        error = np.linalg.norm(x-x0) / np.linalg.norm(x)
         x0 = x.copy()
         it += 1
 
@@ -33,5 +58,10 @@ if __name__ == '__main__':
     A, b = utils.load_all()
     # checks
     assert checks.is_diagonally_dominant(A[2])
+    assert checks.is_symmetrical(A[2]) and checks.is_positive_definite(A[2])
+    assert checks.is_symmetrical(A[3]) and checks.is_positive_definite(A[3])
     # jacobi
     _, _ = jacobi(A[2], b[2], np.zeros_like(b[2]), 1e-16, np.inf)
+    # gauss-seidel
+    _, _ = gauss_seidel(A[2], b[2], np.zeros_like(b[2]), 1e-16, np.inf)
+    _, _ = gauss_seidel(A[3], b[3], np.ones_like(b[3]), 1e-16, 100)
