217e6c52a6b6b863020bcff30fd0e1411f9d5ba2
Daniele Muffato <danixgithub1@gmail.com>
2024-07-19 11:55:27 +0200

    adding gauss-seidel acc

--- metodi_numerici/init.py
+++ metodi_numerici/init.py
@@ -1,5 +1,37 @@
 #!/bin/python3
 
 from lib import utils, stats, checks, solve_triangular
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def spectral_radius(T):
+    spectral_radius = np.max(np.linalg.eigvals(T))
+    return spectral_radius
+
+
+def omega_matrix(D, E, F, w):
+    T = np.linalg.inv(D + w * E) @ ((1 - w) * D - w * F)
+    return T
+
+
+def omega_spectral_radius(D, E, F, w):
+    return spectral_radius(omega_matrix(D, E, F, w))
+
 
 A, b = utils.load_all()
+A = A[3]
+D = np.diag(np.diag(A))
+E = np.tril(A, -1)
+F = np.triu(A, 1)
+Tj = np.linalg.inv(D)@(-E-F)
+Tg = np.linalg.inv(D+E)@(-F)
+
+spectral_radius(Tj)
+spectral_radius(Tg)
+
+X = np.linspace(1.30, 1.35, 50)
+Y = np.array([omega_spectral_radius(D, E, F, w) for w in X])
+plt.plot(X, Y, 's')
+plt.show()
+print(np.min(Y))
--- metodi_numerici/scomposition.py
+++ metodi_numerici/scomposition.py
@@ -56,14 +56,44 @@ def gauss_seidel(A, b, x0, toll, itmax):
     return x, it
 
 
+def gauss_seidel_acc(A, b, x0, toll, itmax, omega):
+    D = np.diag(np.diag(A))
+    E = np.tril(A, -1)
+    F = np.triu(A, 1)
+    M = D + E
+    invM = np.linalg.inv(M)
+    N = -F
+    T = invM @ N
+    it = 1
+    error = np.inf
+
+    # check for convergence
+    print("gauss_seidel acc spectral radius: ", np.max(np.linalg.eigvals(T)))
+    if (np.max(np.linalg.eigvals(T)) >= 1):
+        raise ValueError("Gauss-seidel cannot converge")
+
+    # iterate toward the solution
+    while it < itmax and error > toll:
+        x, _ = solve_triangular.Lsolve(M, N@x0+b)
+        error = np.linalg.norm(x-x0) / np.linalg.norm(x)
+        x0 = x.copy()
+        it += 1
+
+    return x, it
+
+
 if __name__ == '__main__':
     A, b = utils.load_all()
+    inf = np.inf
     # checks
     assert checks.is_diagonally_dominant(A[2])
     assert checks.is_symmetrical(A[2]) and checks.is_positive_definite(A[2])
     assert checks.is_symmetrical(A[3]) and checks.is_positive_definite(A[3])
     # jacobi
-    _, _ = jacobi(A[2], b[2], np.zeros_like(b[2]), 1e-15, np.inf)
+    _, _ = jacobi(A[2], b[2], np.zeros_like(b[2]), 1e-15, inf)
     # gauss-seidel
-    _, _ = gauss_seidel(A[2], b[2], np.zeros_like(b[2]), 1e-15, np.inf)
-    _, _ = gauss_seidel(A[3], b[3], np.ones_like(b[3]), 1e-15, np.inf)
+    _, _ = gauss_seidel(A[2], b[2], np.zeros_like(b[2]), 1e-10, inf)
+    _, _ = gauss_seidel(A[3], b[3], np.zeros_like(b[3]), 1e-4, inf)
+    # gauss-seidel accelerated
+    _, _ = gauss_seidel_acc(A[2], b[2], np.zeros_like(b[2]), 1e-10, inf, 1.67)
+    _, _ = gauss_seidel_acc(A[3], b[3], np.zeros_like(b[3]), 1e-4, inf, 1.33)
