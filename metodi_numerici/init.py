15b7bc8727183cf8b8cb8d5e43531c09d2f50b8d
Daniele Muffato <danixgithub1@gmail.com>
2024-07-28 16:25:50 +0200

    ctrl+c now skips current animation

--- metodi_numerici/init.py
+++ metodi_numerici/init.py
@@ -15,6 +15,14 @@ falopts = ['vert', 'falsi']
 coropts = ['vert', 'corde']
 
 
+def anim(name, vecx, a, b, x0, opts):
+    print(f"animation {name}")
+    try:
+        roots.animate(func, vecx, a, b, x0, opts)
+    except KeyboardInterrupt:
+        print("skipping animation...")
+
+
 _, _, bis1 = roots.bisection(func, 1.5, 2.5, error)
 _, _, bis2 = roots.bisection(func, 2.5, 3.5, error)
 _, _, bis3 = roots.bisection(func, 3.5, 4.5, error)
@@ -27,15 +35,15 @@ _, _, cor1 = roots.corde(func, corm(func, 1.5, 2.5), 1.5, error, error, itmax)
 _, _, cor2 = roots.corde(func, corm(func, 2.5, 3.5), 2.5, error, error, itmax)
 _, _, cor3 = roots.corde(func, corm(func, 3.5, 4.5), 3.5, error, error, itmax)
 _, _, cor4 = roots.corde(func, corm(func, 4.5, 5.2), 4.5, error, error, itmax)
-roots.animate(func, bis1, 1.5, 2.5, opts=bisopts)
-roots.animate(func, bis2, 2.5, 3.5, opts=bisopts)
-roots.animate(func, bis3, 3.5, 4.5, opts=bisopts)
-roots.animate(func, bis4, 4.5, 5.2, opts=bisopts)
-roots.animate(func, fal1, 1.5, 2.5, opts=falopts)
-roots.animate(func, fal2, 2.5, 3.5, opts=falopts)
-roots.animate(func, fal3, 3.5, 4.5, opts=falopts)
-roots.animate(func, fal4, 4.5, 5.2, opts=falopts)
-roots.animate(func, cor1, 1.5, 2.5, x0=1.5, opts=coropts)
-roots.animate(func, cor2, 2.5, 3.5, x0=2.5, opts=coropts)
-roots.animate(func, cor3, 3.5, 4.5, x0=3.5, opts=coropts)
-roots.animate(func, cor4, 4.5, 5.2, x0=4.5, opts=coropts)
+anim('bisection 1', bis1, 1.5, 2.5, None, bisopts)
+anim('bisection 2', bis2, 2.5, 3.5, None, bisopts)
+anim('bisection 3', bis3, 3.5, 4.5, None, bisopts)
+anim('bisection 4', bis4, 4.5, 5.2, None, bisopts)
+anim('falsi 1', fal1, 1.5, 2.5, None, falopts)
+anim('falsi 2', fal2, 2.5, 3.5, None, falopts)
+anim('falsi 3', fal3, 3.5, 4.5, None, falopts)
+anim('falsi 4', fal4, 4.5, 5.2, None, falopts)
+anim('corde 1', cor1, 1.5, 2.5, 1.5, coropts)
+anim('corde 2', cor2, 2.5, 3.5, 2.5, coropts)
+anim('corde 3', cor3, 3.5, 4.5, 3.5, coropts)
+anim('corde 4', cor4, 4.5, 5.2, 4.5, coropts)
