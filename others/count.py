63506eb5aaed69b10bddef0c694de4fa3b92a446
Daniele Muffato <danixgithub1@gmail.com>
2024-08-14 01:58:37 +0200

    small stuff

--- others/count.py
+++ others/count.py
@@ -1,6 +1,10 @@
+# counting
 count = 0
 for i in range(1, 101):
     if i % 2 == 0 or i % 3 == 0 or i % 5 == 0:
         count += 1
 
-print(count)
+# square sum
+for i in range(1, 5):
+    for j in range(1, 5):
+        print(i**2+j**2)
