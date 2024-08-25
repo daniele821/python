95fd1846b5f7eac7d74c788a2b4efa7c8aa1b481
Daniele Muffato <danixgithub1@gmail.com>
2024-08-25 18:20:39 +0200

    small impr

--- probabilita/probability.py
+++ probabilita/probability.py
@@ -1,15 +1,15 @@
-import combinatorics as cmb
+from combinatorics import binomial_coefficient as bc
 from fractions import Fraction
 
 
 def density_binomial(n, p, k):
     if isinstance(p, str):
         p = Fraction(p)
-    return cmb.binomial_coefficient(n, k) * (p**k) * ((1-p)**(n-k))
+    return bc(n, k) * (p**k) * ((1-p)**(n-k))
 
 
 def density_ipergeometric(n, b, r, k):
-    return Fraction(cmb.binomial_coefficient(b, k) * cmb.binomial_coefficient(r, n - k)) / Fraction(cmb.binomial_coefficient(r + b, n))
+    return Fraction(bc(b, k) * bc(r, n - k)) / Fraction(bc(r + b, n))
 
 
 print(density_binomial(100, '0.85', 85).__float__())
