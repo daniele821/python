7de8b78556941e47c9caf359f8d458fd65d68b2e
Daniele Muffato <danixgithub1@gmail.com>
2024-10-05 17:13:26 +0200

    improving simplex solver

--- ricerca_operativa/simplex_solver.py
+++ ricerca_operativa/simplex_solver.py
@@ -1,9 +1,9 @@
-#!/bin/python3
+#!/bin/env python3
 
 from scipy.optimize import linprog
 
 
-def solve_file(file):
+def solve_file_v1(file):
     with open(file, "r") as fp:
         lines = fp.read().splitlines()
     lines = [elem for elem in lines if elem.strip()]
@@ -29,8 +29,6 @@ def solve_file(file):
         for sign in ("==", "<=", ">="):
             if sign in elems:
                 sign_index = elems.index(sign)
-        if not sign_index:
-            raise ValueError("invalid input file!")
         buffer1 = elems[:sign_index]
         buffer2 = elems[sign_index + 1]
         sign = elems[sign_index]
@@ -54,7 +52,48 @@ def solve_file(file):
     print(linprog(obj, dis_lhs, dis_rhs, eq_lhs, eq_rhs))
 
 
-solve_file('./input.txt')
+def parse_linear(vars, invert, linear):
+    # return vector of linear function coefficients
+    pass
 
-# can only solve MINIMIZATION problems
-# can only have <= disequations, or == equations
+
+def solve_file_v2(file):
+    with open(file, "r") as fp:
+        lines = fp.read().splitlines()
+    lines = [elem for elem in lines if elem.strip()]
+    vars = lines[0].split()[1:]
+    obj = lines[1].split()
+    matrix = lines[2:]
+    dis_lhs = []
+    dis_rhs = []
+    eq_lhs = []
+    eq_rhs = []
+
+    # parsing and checking object function
+    obj = parse_linear(vars, obj[0] == "max", "".join(obj[1:]))
+
+    # parsing and checking matrix
+    for line in matrix:
+        index = line.find("=")
+        if line[index-1] in "<>":
+            index -= 1
+        linear = "".join(line[:index].split())
+        lhs = parse_linear(vars, line[index] == ">", linear)
+        rhs = float(line[index+2:])
+        if line[index] == "=":
+            eq_lhs.append(lhs)
+            eq_rhs.append(rhs)
+        else:
+            dis_lhs.append(lhs)
+            dis_rhs.append(rhs)
+    if not dis_lhs or not dis_rhs:
+        dis_lhs = None
+        dis_rhs = None
+    if not eq_lhs or not eq_rhs:
+        eq_lhs = None
+        eq_rhs = None
+    print(eq_lhs, eq_rhs, dis_lhs, dis_rhs)
+
+
+# solve_file_v1('./input_v1.txt')
+solve_file_v2('./input_v2.txt')
