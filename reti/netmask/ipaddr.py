183b69d01071f1a744b2bfc30e37133a29334664
Daniele Muffato <danixgithub1@gmail.com>
2024-10-10 18:24:55 +0200

    added this wifi ip getter (very hardcoded in)

--- reti/netmask/ipaddr.py
+++ reti/netmask/ipaddr.py
@@ -2,12 +2,15 @@
 
 import sys
 import lib
-
+import subprocess
 # input: ip [/netmask]
 # output: ip, netmask, net id, host id range
 
 if __name__ == "__main__":
     input = ''.join(''.join(sys.argv[1:]).split())
+    if not input:
+        input = subprocess.getoutput(
+            "nmcli dev show wlp1s0 | grep -i 'ip4.address'").split()[1]
     sep_index = input.find('/')
     ipstr = input[:sep_index] if sep_index != -1 else input
     netmaskstr = input[sep_index+1:] if sep_index != -1 else ''
@@ -18,6 +21,9 @@ if __name__ == "__main__":
     host_ip = lib.bit_operation(ip, netmask, lambda x, y: x & y)
     broadcast = lib.bit_operation(ip, netmask, lambda x, y: x | (~y & 0xff))
 
+    print("IP and netmask passed are: " +
+          lib.ip_to_str(ip).strip() + "/" + netmaskstr)
+    print()
     print("ip address | " + lib.ip_to_str(ip) +
           " | " + lib.binary_ip_to_str(ip))
     print("netmask    | " + lib.ip_to_str(netmask) +
