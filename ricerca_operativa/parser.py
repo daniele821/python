aa6e761003a4c4b51df8d4f72d30f5f4e8b1b373
Daniele Muffato <danixgithub1@gmail.com>
2024-10-11 21:14:50 +0200

    wip

--- ricerca_operativa/parser.py
+++ ricerca_operativa/parser.py
@@ -1,7 +1,5 @@
 #!/bin/env python3
 
-from scipy.optimize import linprog
-
 
 def parse_linear(vars, linear):
     coeff = [0] * len(vars)
@@ -24,55 +22,40 @@ def parse_linear(vars, linear):
     return coeff
 
 
-def solve_file(input):
+def parse_file(input):
     lines = input.splitlines()
-    lines = [e for e in lines if e.strip() and not e.startswith("//")]
+    lines = [e.strip() for e in lines if e.strip() and not e.startswith("//")]
 
-    # TODO
-    vars = lines[0].split()[1:]
-    obj = lines[1].split()[1:]
-    matrix = lines[2:]
+    properties = set()
+    vars = []
+    obj = []
+    matrixes_lhs = [[], [], []]
+    matrixes_rhs = [[], [], []]
 
-    dis_lhs = []
-    dis_rhs = []
-    eq_lhs = []
-    eq_rhs = []
+    status = 0
+    for line in lines:
+        if status == 0:
+            if line.startswith("vars"):
+                status += 1
+            else:
+                properties.add(line)
+        if status == 1:
+            if not vars:
+                vars = line.split()[1:]
+            else:
+                status += 1
+        if status == 2:
+            if not obj:
+                obj = parse_linear(vars, "".join(line.split()[1:]))
+            else:
+                status += 1
+        if status == 3:
+            sign_index = line.rfind("=")
+            match line[sign_index-1]:
+                case "<": pos = 0
+                case ">": pos = 1
+                case "=": pos = 2
+            matrixes_lhs[pos].append(parse_linear(vars, line[:sign_index-1]))
+            matrixes_rhs[pos].append(float(line[sign_index+1:]))
 
-    is_unbounded = "unbound" in lines[0].split()[0]
-    is_continuos = "int" not in lines[0].split()[0]
-    is_min = lines[1].split() == "min"
-    prop = []
-
-    # parsing stuff
-    prop.append((0, None) if not is_unbounded else (None, None))
-    prop.append(0 if is_continuos else 1)
-
-    # parsing and checking object function
-    obj = parse_linear(vars, "".join(obj[1:]))
-
-    # parsing and checking matrix
-    for line in matrix:
-        index = line.find("=")
-        if line[index-1] in "<>":
-            index -= 1
-        linear = "".join(line[:index].split())
-        lhs = parse_linear(vars, line[index] == ">", linear)
-        rhs = float(line[index+2:])
-        if line[index] == ">":
-            rhs *= -1
-        if line[index] == "=":
-            eq_lhs.append(lhs)
-            eq_rhs.append(rhs)
-        else:
-            dis_lhs.append(lhs)
-            dis_rhs.append(rhs)
-    if not dis_lhs or not dis_rhs:
-        dis_lhs = None
-        dis_rhs = None
-    if not eq_lhs or not eq_rhs:
-        eq_lhs = None
-        eq_rhs = None
-    linsol = linprog(obj, dis_lhs, dis_rhs, eq_lhs,
-                     eq_rhs, prop[0], integrality=prop[1])
-
-    return linsol
+    return properties, vars, obj, matrixes_lhs, matrixes_rhs
