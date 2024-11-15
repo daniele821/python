9f768228faefd9b7abfd542dec91d5debd9b6d9e
Daniele Muffato <danixgithub1@gmail.com>
2024-11-15 17:46:44 +0100

    graph coloring problem

--- ricerca_operativa/coloring/load.py
+++ ricerca_operativa/coloring/load.py
@@ -1,1 +1,23 @@
 #!/bin/python3
+
+
+def load(file):
+    txt = open(file, 'r').read()
+    graph = {}
+    for line in txt.splitlines():
+        nodes = line.split()
+        start = int(nodes[0])
+        end = int(nodes[1])
+        if start not in graph:
+            graph[start] = set()
+        graph[start].add(end)
+        if end not in graph:
+            graph[end] = set()
+        graph[end].add(start)
+    return graph
+
+
+graph = load('graph.txt')
+
+for i in sorted(graph):
+    print(i, len(graph[i]))
