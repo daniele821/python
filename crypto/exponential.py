751a926572e53d737f639cc2395e0175b086adad
Daniele Muffato <danixgithub1@gmail.com>
2025-03-13 12:15:34 +0100

    performance decorator

--- crypto/exponential.py
+++ crypto/exponential.py
@@ -1,9 +1,20 @@
 #!/bin/python3
 
-import sys
+import sys, time
 
 sys.set_int_max_str_digits(10**9)
-# a^b mod c
+
+
+def performance_timer(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()  # Start the timer
+        result = func(*args, **kwargs)  # Call the function
+        end_time = time.perf_counter()  # End the timer
+        elapsed_time = end_time - start_time  # Calculate elapsed time
+        print(f"Function '{func.__name__}' executed in {elapsed_time:.6f} seconds")
+        return result  # Return the original function result
+
+    return wrapper
 
 
 def mod(num, mod):
@@ -12,6 +23,7 @@ def mod(num, mod):
     return num
 
 
+@performance_timer
 def exp(a, b, c):
     res = a
     for i in reversed(range(b.bit_length() - 1)):
