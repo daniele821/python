f59b1628e1758e107721091220ae1fe9653340a8
Daniele Muffato <danixgithub1@gmail.com>
2025-03-13 17:39:19 +0100

    stronger decorator

--- crypto/rsa.py
+++ crypto/rsa.py
@@ -6,23 +6,26 @@ import time
 
 sys.set_int_max_str_digits(10**9)
 
-DEBUG = True
+DEBUG_LEVEL = 1000
 
 
-def performance_timer(func):
-    def wrapper(*args, **kwargs):
-        start_time = time.perf_counter()
-        result = func(*args, **kwargs)
-        end_time = time.perf_counter()
-        elapsed_time = end_time - start_time
-        if DEBUG:
-            print(f"Function '{func.__name__}' executed in {elapsed_time:.6f} seconds")
-        return result
+def performance_timer(dbgLvl=1000):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            start_time = time.perf_counter()
+            result = func(*args, **kwargs)
+            end_time = time.perf_counter()
+            elapsed_time = end_time - start_time
+            if dbgLvl >= DEBUG_LEVEL:
+                print(f"Function '{func.__name__}' executed in {elapsed_time:.6f} seconds")
+            return result
 
-    return wrapper
+        return wrapper
+
+    return decorator
 
 
-@performance_timer
+@performance_timer()
 def exp(a, b, c):
     res = a
     for i in reversed(range(b.bit_length() - 1)):
@@ -33,15 +36,15 @@ def exp(a, b, c):
     return res
 
 
-@performance_timer
+@performance_timer()
 def rand_ndigit_number(n):
     return random.randint(10 ** (n - 1), 10**n - 1)
 
 
-@performance_timer
+@performance_timer()
 def test_prime(n, rounds=10):
     pass
 
 
 if __name__ == "__main__":
-    pass
+    print(exp(11, 2, 1000))
